3b4fdb62be038b661b1aaca0216d578485b78542
Kim Brill <kbrill@gmx.net>
2019-01-30 11:25:22 +0100

    db_create.py

--- db_create.py
+++ db_create.py
@@ -1,9 +1,9 @@
-from routes import db
-from models import User
+
+from models import db, User
 
 #create database and tables
 db.create_all()
 
-db.session.add(Users("kim", 'kim@kim.com', 'hallo'))
+db.session.add(Users("kim", 'kim@kim.kimkim', 'hallo'))
 
 db.session.commit()
